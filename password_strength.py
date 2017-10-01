5d6ea0c31ef26e06e0b1a59ebcadfd540ed973b1
lucky <lucky@lucky.ilbirs.com>
2017-10-01 04:34:26 +0300

    check fixes

--- password_strength.py
+++ password_strength.py
@@ -2,8 +2,10 @@ import re
 
 
 def get_password_strength(password):
-    blacklist = list(map(lambda x: x.strip(),
-                         tuple(open('500-worst-passwords.txt', 'r'))))
+    with open('500-worst-passwords.txt', 'r') as blacklist_file:
+        blacklist = list(map(lambda x: x.strip(),
+                             tuple(blacklist_file.readlines())))
+
     strength = 1
 
     if len(password) > 6:
@@ -12,18 +14,16 @@ def get_password_strength(password):
                         any(c.isdigit() for c in password) + \
                         any(c.isalpha() for c in password)
         base_strength += 1 if base_strength == 4 else 0
-        rpass = re.compile(password)
 
         dict_strength = 1
         if password in blacklist:
             dict_strength = -3
-        elif any(re.search(word, password) is not None for word in blacklist):
+        elif any(len(word)>3 and word in password for word in blacklist):
             dict_strength = -2
 
         strength = strength + base_strength + dict_strength + \
-                   len(re.findall(r'\,\.\!\@\#\&\$', password)) + \
+                   len(re.findall(r'[\,\.\!\@\#\&\$]', password)) + \
                    (1 if len(password) > 10 else 0)
-
         if strength > 10:
             strength = 10
         if strength <= 0:
