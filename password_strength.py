403920195ccae29f99b0a18b1348a19209f4698f
Alex <lucky_is@mail.ru>
2017-10-02 18:06:38 +0300

    pep8 fixes

--- password_strength.py
+++ password_strength.py
@@ -1,6 +1,7 @@
 import re
 import getpass
 
+
 def get_password_strength(password):
     with open('500-worst-passwords.txt', 'r') as blacklist_file:
         blacklist = list(map(lambda x: x.strip(),
@@ -9,21 +10,23 @@ def get_password_strength(password):
     strength = 1
 
     if len(password) > 6:
-        base_strength = any(c.islower() for c in password) + \
-                        any(c.isupper() for c in password) + \
-                        any(c.isdigit() for c in password) + \
-                        any(c.isalpha() for c in password)
+        base_strength = (any(c.islower() for c in password) +
+                         any(c.isupper() for c in password) +
+                         any(c.isdigit() for c in password) +
+                         any(c.isalpha() for c in password))
         base_strength += 1 if base_strength == 4 else 0
 
         dict_strength = 1
         if password in blacklist:
             dict_strength = -3
-        elif any(len(word)>3 and word in password for word in blacklist):
+        elif any(len(word) > 3 and word in password for word in blacklist):
             dict_strength = -2
 
-        strength = strength + base_strength + dict_strength + \
-                   len(re.findall(r'[\,\.\!\@\#\&\$]', password)) + \
-                   (1 if len(password) > 10 else 0)
+        strength = (strength +
+                    base_strength +
+                    dict_strength +
+                    len(re.findall(r'[\,\.\!\@\#\&\$]', password)) +
+                    (1 if len(password) > 10 else 0))
 
         if not 0 < strength < 11:
             strength = 10 if strength > 10 else 1
