a440bbf4d41a3875a78f76bfff1906eb4c1bac71
Alex <lucky_is@mail.ru>
2017-10-04 18:29:15 +0300

    split to simple functions

--- password_strength.py
+++ password_strength.py
@@ -2,29 +2,49 @@ import re
 import getpass
 
 
-def get_password_strength(password):
-    with open('500-worst-passwords.txt', 'r') as blacklist_file:
-        blacklist = list(map(lambda x: x.strip(),
-                             tuple(blacklist_file.readlines())))
+def read_worst_passwords():
+    blacklist = []
+    try:
+        with open('500-worst-passwords.txt', 'r') as blacklist_file:
+            blacklist = list(map(lambda x: x.strip(),
+                                 tuple(blacklist_file.readlines())))
+    except IOError:
+        print("File '500-worst-passwords.txt' is not found")
 
+    return blacklist
+
+
+def get_dict_strength(password):
+    blacklist = read_worst_passwords()
+    dict_strength = 1
+
+    if len(blacklist) == 0:
+        dict_strength = 0
+    elif password in blacklist:
+        dict_strength = -3
+    elif any(len(word) > 3 and word in password for word in blacklist):
+        dict_strength = -2
+
+    return dict_strength
+
+
+def get_base_strength(password):
+    base_strength = (any(c.islower() for c in password) +
+                     any(c.isupper() for c in password) +
+                     any(c.isdigit() for c in password) +
+                     any(c.isalpha() for c in password))
+    base_strength += 1 if base_strength == 4 else 0
+
+    return base_strength
+
+
+def get_password_strength(password):
     strength = 1
 
     if len(password) > 6:
-        base_strength = (any(c.islower() for c in password) +
-                         any(c.isupper() for c in password) +
-                         any(c.isdigit() for c in password) +
-                         any(c.isalpha() for c in password))
-        base_strength += 1 if base_strength == 4 else 0
-
-        dict_strength = 1
-        if password in blacklist:
-            dict_strength = -3
-        elif any(len(word) > 3 and word in password for word in blacklist):
-            dict_strength = -2
-
         strength = (strength +
-                    base_strength +
-                    dict_strength +
+                    get_base_strength(password) +
+                    get_dict_strength(password) +
                     len(re.findall(r'[\,\.\!\@\#\&\$]', password)) +
                     (1 if len(password) > 10 else 0))
 
@@ -35,5 +55,5 @@ def get_password_strength(password):
 
 
 if __name__ == '__main__':
-    password = getpass.getpass("input your password for check:")
-    print('Your password strength is %d' % get_password_strength(password))
+    in_password = getpass.getpass("input your password for check:")
+    print('Your password strength is %d' % get_password_strength(in_password))
