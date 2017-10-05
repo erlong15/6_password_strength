e0d7beb303d7c0eb070c179d5ce0c65a4e75234d
Alex <lucky_is@mail.ru>
2017-10-05 19:32:56 +0300

    name changes

--- password_strength.py
+++ password_strength.py
@@ -3,48 +3,46 @@ import getpass
 
 
 def read_worst_passwords():
-    blacklist = []
     try:
         with open('500-worst-passwords.txt', 'r') as blacklist_file:
-            blacklist = list(map(lambda x: x.strip(),
-                                 tuple(blacklist_file.readlines())))
+            black_list = list(map(lambda x: x.strip(),
+                                  tuple(blacklist_file.readlines())))
     except IOError:
-        print("File '500-worst-passwords.txt' is not found")
+        black_list = []
 
-    return blacklist
-
-
-def get_dict_strength(password):
-    blacklist = read_worst_passwords()
-    dict_strength = 1
-
-    if not blacklist:
-        dict_strength = 0
-    elif password in blacklist:
-        dict_strength = -3
-    elif any(len(word) > 3 and word in password for word in blacklist):
-        dict_strength = -2
-
-    return dict_strength
+    return black_list
 
 
-def get_base_strength(password):
-    base_strength = (any(c.islower() for c in password) +
-                     any(c.isupper() for c in password) +
-                     any(c.isdigit() for c in password) +
-                     any(c.isalpha() for c in password))
-    base_strength += 1 if base_strength == 4 else 0
+def get_blacklist_check_points(password, in_blacklist):
+    check_points = 1
 
-    return base_strength
+    if not in_blacklist:
+        check_points = 0
+    elif password in in_blacklist:
+        check_points = -3
+    elif any(len(word) > 3 and word in password for word in in_blacklist):
+        check_points = -2
+
+    return check_points
 
 
-def get_password_strength(password):
+def get_alphanum_check_points(password):
+    check_points = (any(c.islower() for c in password) +
+                    any(c.isupper() for c in password) +
+                    any(c.isdigit() for c in password) +
+                    any(c.isalpha() for c in password))
+    check_points += 1 if check_points == 4 else 0
+
+    return check_points
+
+
+def get_password_strength(password, in_blacklist):
     strength = 1
 
     if len(password) > 6:
         strength = (strength +
-                    get_base_strength(password) +
-                    get_dict_strength(password) +
+                    get_alphanum_check_points(password) +
+                    get_blacklist_check_points(password, in_blacklist) +
                     len(re.findall(r'[\,\.\!\@\#\&\$]', password)) +
                     (1 if len(password) > 10 else 0))
 
@@ -56,4 +54,9 @@ def get_password_strength(password):
 
 if __name__ == '__main__':
     in_password = getpass.getpass("input your password for check:")
-    print('Your password strength is %d' % get_password_strength(in_password))
+    blacklist = read_worst_passwords()
+    if not blacklist:
+        print("File '500-worst-passwords.txt' is not found or empty")
+
+    print('Your password strength is %d' % get_password_strength(in_password,
+                                                                 blacklist))
